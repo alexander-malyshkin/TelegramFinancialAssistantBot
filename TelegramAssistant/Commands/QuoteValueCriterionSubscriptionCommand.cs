8600443375856e0c6e83954e4bb21ac2df2bcc95
alex <alexander.malyshkin@smartseeds.ru>
2019-08-09 23:41:27 +0300

    реализовал QuoteValueCriterionSubscriptionCommand

--- TelegramAssistant/Commands/QuoteValueCriterionSubscriptionCommand.cs
+++ TelegramAssistant/Commands/QuoteValueCriterionSubscriptionCommand.cs
@@ -11,27 +11,72 @@ namespace TelegramAssistant.Commands
     internal class QuoteValueCriterionSubscriptionCommand : ICommand<QuoteValueCriterionSubscriptionResponse>
     {
         private readonly QuoteValueCriterionSubscriptionRequest _request;
+        private readonly INotificationSubscriber _notificationSubscriber;
+        private readonly IExchangeRatesProvider _exchangeRatesProvider;
 
-        public QuoteValueCriterionSubscriptionCommand(QuoteValueCriterionSubscriptionRequest request)
+        private string _asset;
+
+        public QuoteValueCriterionSubscriptionCommand(QuoteValueCriterionSubscriptionRequest request, 
+            INotificationSubscriber notificationSubscriber, IExchangeRatesProvider exchangeRatesProvider)
         {
-            _request = request;
+            _request = request ?? throw new ArgumentException($"Не указан запрос {nameof(QuoteValueCriterionSubscriptionRequest)}");
+            _notificationSubscriber = notificationSubscriber ?? throw new ArgumentException($"Не указан {nameof(INotificationSubscriber)}");
+            _exchangeRatesProvider = exchangeRatesProvider;
         }
 
         public async Task<bool> Validate()
         {
 
-            var supportedAssets = Enum.GetNames(typeof(Asset));
+            var supportedAssets = await _exchangeRatesProvider.GetAssets();
             if (supportedAssets.All(supportedAsset => !_request.Asset.StartsWith(supportedAsset)))
             {
                 return false;
             }
 
+            _asset = _request.Asset.Split(_request.Operator).First();
             return true;
         }
 
         public async Task<QuoteValueCriterionSubscriptionResponse> Process()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var canSubscribe = await _notificationSubscriber.CanSubscribe(_asset, _request.Predicate);
+                if(!canSubscribe)
+                {
+                    return new QuoteValueCriterionSubscriptionResponse
+                    {
+                        Success = true,
+                        ResultMessage = "Указанный актив уже удовлетворяет условию"
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                return new QuoteValueCriterionSubscriptionResponse
+                {
+                    Success = false,
+                    ResultMessage = "Не получилось проверить выполнение условия по указанному активу"
+                };
+            }
+
+            try
+            {
+                await _notificationSubscriber.Subscribe(_asset, _request.Predicate);
+                return new QuoteValueCriterionSubscriptionResponse
+                {
+                    Success = true,
+                    ResultMessage = "Вы успешно подписались на событие"
+                };
+            }
+            catch (Exception e)
+            {
+                return new QuoteValueCriterionSubscriptionResponse
+                {
+                    Success = false,
+                    ResultMessage = $"Не удалось подписаться на событие"
+                };
+            }
         }
     }
 }
