1fe73bdc425debe25002a3ba11b01471f0f303e9
alex <alexander.malyshkin@smartseeds.ru>
2019-08-10 21:50:46 +0300

    убрал неиспользуемый фоновый процесс QueuedHostedService

--- TelegramAssistant/Program.cs
+++ TelegramAssistant/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Design;
-using System.Security.Authentication.ExtendedProtection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Telegram.Bot;
 using TelegramAssistant.Contracts;
 using TelegramAssistant.ExchangeRateProviders;
 using TelegramAssistant.NotificationSubscribers;
-using TelegramAssistant.Services;
 
 namespace TelegramAssistant
 {
@@ -23,8 +17,7 @@ namespace TelegramAssistant
             //    new NotificationSubscriber(new MockSberbankExchangeRatesProvider()));
 
             var sc2 = new ServiceContainer();
-            sc2.AddService(typeof(QueuedHostedService), new QueuedHostedService(new BackgroundTaskQueue()));
-            sc2.AddService(typeof(IBackgroundTaskQueue), new BackgroundTaskQueue());
+            
             sc2.AddService(typeof(IExchangeRatesProvider), new MockExchangeRatesProvider());
             sc2.AddService(typeof(INotificationSubscriber), new NotificationSubscriber(new MockExchangeRatesProvider()));
             
