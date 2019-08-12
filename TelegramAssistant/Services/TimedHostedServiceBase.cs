2cf8f984129af58c08a48d32b2f34f46a1e5effb
alex <alexander.malyshkin@smartseeds.ru>
2019-08-13 00:49:22 +0300

    добавил в проект фоновый процесс, срабатывающий по интервалу времени

---
+++ TelegramAssistant/Services/TimedHostedServiceBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace TelegramAssistant.Services
+{
+    internal abstract class TimedHostedServiceBase : IHostedService, IDisposable
+    {
+        private Timer _timer;
+        private readonly int _intervalMs;
+        private readonly int _dueTimeSpanSeconds;
+
+        protected TimedHostedServiceBase(int intervalMs, int dueTimeSpanSeconds)
+        {
+            _intervalMs = intervalMs;
+            _dueTimeSpanSeconds = dueTimeSpanSeconds;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+
+            _timer = new Timer(DoWork, null, TimeSpan.FromSeconds(_dueTimeSpanSeconds),
+                TimeSpan.FromMilliseconds(_intervalMs));
+
+            return Task.CompletedTask;
+        }
+
+        protected abstract void DoWork(object state);
+
+        
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            //_logger.LogInformation("Timed Background Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
