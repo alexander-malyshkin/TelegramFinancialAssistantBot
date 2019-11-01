eee0816d3b351b638357992898e8991d10b69ee1
alex <alexander.malyshkin@smartseeds.ru>
2019-11-01 15:26:17 +0300

    introduced separate class with reusable strings

---
+++ TelegramAssistant/MessageTexts.cs
@@ -0,0 +1,15 @@
+﻿namespace TelegramAssistant
+{
+    public class MessageTexts
+    {
+        public const string InternalException = "Something went wrong";
+        public const string NotImplementedEception = "This functionality isn't supported. Please, contact the developers";
+        public const string NotSpecified = "Not specified";
+        public const string ConditionAlreadySatisfied = "Condition is already is satisfied, current value is {0}";
+        public const string AlreadySubscribed = "Already subscribed";
+        public const string ConditionCheckFailed = "Could not check the condition";
+        public const string SubscribedSuccessfully = "You have successfully subscribed to the event, current value is {0}";
+        public const string SubscriptionFailed = "Could not create the subscription";
+        public const string AssetCurrentValue = "Актив {0} теперь имеет цену {1}";
+    }
+}
